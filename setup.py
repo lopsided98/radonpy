d879a8594dd0b0baba7895fe1866744579fb3f57
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-06-07 20:21:16 -0400

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='radonpy',
-    version='0.1.0',
-    author='Ben Wolsieffer',
-    author_email='benwolsieffer@gmail.com',
-    description='Tools to communicate with the RadonEye RD200 radon detector',
-    license='Apache License 2.0',
-    keywords=['radon' 'radoneye'],
-    url='http://packages.python.org/radonpy',
-    long_description=open('README.md').read(),
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Topic :: Home Automation',
-        'License :: OSI Approved :: Apache Software License',
-    ],
-    install_requires=[
-        'bleak',
-        'aioinflux'
-    ],
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': ['radonpy=radonpy.main:main'],
-    }
-)
+setup()
