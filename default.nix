17930248331eeebe5572177dd72fca0390b55d61
Ben Wolsieffer <benwolsieffer@gmail.com>
2026-01-24 20:34:49 -0500

    Update nixpkgs.
    aioinflux has been removed from nixpkgs because it is unmaintained
    upstream. There is no reasonable replacement for it though so just add
    the package definition locally.

--- default.nix
+++ default.nix
@@ -1,15 +1,61 @@
 {
   lib,
+  callPackage,
   fetchFromGitHub,
   buildPythonApplication,
   setuptools,
-  aioinflux,
   bleak,
   black,
   flake8,
   mypy,
 }:
+let
+  aioinflux = callPackage (
+    {
+      lib,
+      buildPythonPackage,
+      fetchPypi,
+      aiohttp,
+      ciso8601,
+      setuptools,
+      pandas,
+    }:
 
+    buildPythonPackage rec {
+      pname = "aioinflux";
+      version = "0.9.0";
+      pyproject = true;
+
+      src = fetchPypi {
+        inherit pname version;
+        hash = "sha256-cg0FapBprDaI+Ds1eGsjTIkK+3yaN560IeU3nh6rxcs=";
+      };
+
+      build-system = [ setuptools ];
+
+      dependencies = [
+        aiohttp
+        ciso8601
+        pandas
+      ];
+
+      # Tests require InfluxDB server
+      doCheck = false;
+
+      pythonImportsCheck = [ "aioinflux" ];
+
+      meta = {
+        description = "Asynchronous Python client for InfluxDB";
+        homepage = "https://github.com/gusutabopb/aioinflux";
+        changelog = "https://github.com/gusutabopb/aioinflux/releases/tag/v${version}";
+        license = lib.licenses.mit;
+        maintainers = with lib.maintainers; [
+          lopsided98
+        ];
+      };
+    }
+  ) { };
+in
 buildPythonApplication {
   pname = "radonpy";
   version = "0.2.5";
